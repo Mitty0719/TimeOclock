867e5ebea9f24e7161279df6f7b414a073e8b995
Mitty0719 <sprout071900@gmail.com>
2021-11-24 20:39:43 +0900

    move file for type, and add background image, mouse move event

---
+++ js/mouse.js
@@ -0,0 +1,25 @@
+(function(){
+    const backImage = document.querySelector('.backImage');
+    let x = 0, y = 0;
+    let currentX = 0, currentY = 0;
+    let speed = 0.005;
+
+    function backImageMove(e){
+        x = e.clientX - window.innerWidth / 2;
+        y = e.clientY - window.innerHeight / 2;
+    }
+
+    window.addEventListener('mousemove', backImageMove);
+
+    function backImageMoveRaf(){
+        currentX += (x - currentX) * speed;
+        currentY += (y - currentY) * speed;
+        // console.log(x, currentX);
+        // console.log(y, currentY);
+        backImage.style.transform = 'translate(' + -(currentX/20) + 'px, ' + -(currentY/20) + 'px)';
+        window.requestAnimationFrame(backImageMoveRaf);
+    }
+
+    window.requestAnimationFrame(backImageMoveRaf)
+
+})()
